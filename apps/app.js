d3f201f0554cec530dccf5932fb3678c8376b037
va164 <victor.ahluwalia@gmail.com>
2016-07-16 13:41:36 -0400

    added functionality to view video in a new window on click

--- apps/app.js
+++ apps/app.js
@@ -8,21 +8,24 @@ $(document).ready(function () {
 			},
 			function (data) {
 				displaySearchResults(data.items);
+				console.log(data.items);
 			}
 		);
 	};
 
+
 	function displaySearchResults(videos) {
 		var html = "";
 		$.each(videos, function (index, video) {
-			console.log(video.snippet.thumbnails.medium.url);
 			html = html + "<li><p>" + video.snippet.title +
-				"</p><img src='" +  video.snippet.thumbnails.high.url + "'/></li>" ;
+				"</p><a target='_blank' href='https://www.youtube.com/watch?v=" + video.id.videoId + " '>'<img src='" +  video.snippet.thumbnails.high.url + "'/></li>"
 		});
 		$("#search-results ul").html(html);
 	}
+
 	$("#search-form").submit(function (event) {
 		event.preventDefault();
 		getResults($("#query").val());
 	});
+
 });
